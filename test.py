d06c19a30d2ebc0851c67da4bc9e1205984797c4
Graham Markall <gmarkall@nvidia.com>
2024-05-03 12:26:10 +0100

    Update test.py to test standalone package

--- test.py
+++ test.py
@@ -1,30 +1,8 @@
-import numba_cuda
-import os
-import sys
+#!/usr/bin/env python
 
-# Shadow the numba.cuda module
-
-numba_cuda.install()
-
-from numba import cuda  # noqa: E402
+import numba_cuda as cuda
 
 # Run the test suite
 
 if __name__ == '__main__':
     cuda.test()
-
-    # Ensure we haven't imported anything from the Numba package location by
-    # checking that all numba.cuda imports came from the numba_cuda package's
-    # directory tree
-
-    numba_cuda_path = os.path.dirname(__file__)
-
-    unexpected_path = False
-    for name, module in sys.modules.items():
-        if "numba.cuda" in name:
-            if numba_cuda_path not in module.__file__:
-                print("Warning - unexpected path for "
-                      f"{name}: {module.__file__}")
-                unexpected_path = True
-
-    sys.exit(unexpected_path)
